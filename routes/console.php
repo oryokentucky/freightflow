d60c8715963851fd0f37d59fa02ea6c372ff983e
oryoq <akif-bin-khairul-nizam@webby.my>
2026-07-15 15:12:17 +0800

    fix: updated the setup of the system
    -updated the docker file configuration , added phpstan vendor and adjust composer files

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Services\Outbox\RelayOutboxMessages;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(new RelayOutboxMessages())->everyMinute()->name('relay-outbox-messages')->withoutOverlapping();
